c2bf43dd7bb08b3e5b6e69283577d1ca40cbc869
Naki <alicia.neko@mail.ru>
2025-07-06 10:43:47 -0400

    Update sexo.py test

--- sexo.py
+++ sexo.py
@@ -107,6 +107,7 @@ async def manejar_acto(app, query):
     acto = usuarios_acto[uid]
     callback_query = query #Duplicar la variable porque me sa flojera editar todo
     data = query.data
+    await actualizar_progresos() 
     
     if data == "acto_start":
         generar_menu(uid)
